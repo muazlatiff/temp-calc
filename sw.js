800532e7c3ebfd8d0fe905d694774e2ad5592e38
muazmohdlatiff <muazmohdlatiff@airasia.com>
2023-01-28 02:43:52 +0800

    1. FEAT: Add pwa config files

---
+++ sw.js
@@ -0,0 +1,12 @@
+var GHPATH = '/temp-calc';
+var APP_PREFIX = 'gppwa_';
+ 
+// every time any of the files change, need to change this version (version_01, version_02…)
+var VERSION = 'version_00';
+
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/index.js`
+  `${GHPATH}/ade-logo-white.png`,
+];
