aa613c15b7b1b96bb398d3a04eb038f9c6fdd521
muazmohdlatiff <muazmohdlatiff@airasia.com>
2023-01-31 12:05:38 +0800

    1. FEAT: POC pwa - harcoded everything

--- pwa-temp-calc.js
+++ pwa-temp-calc.js
@@ -3,21 +3,18 @@ var GHPATH = 'temp-calc';
 var APP_PREFIX = 'pwa_temp_calc_';
 
 // every time any of the files change, need to update the version (version_01, version_02…)
-var VERSION = 'version_06';
+var VERSION = 'version_07';
 // every new deployment, need to update the prefix depending on new bundle filename
 var JS_PREFIX = '3b37ebd9';
 // every new deployment, need to update the prefix depending on new bundle filename
 var CSS_PREFIX = '77388d6b';
 
 var URLS = [
-  // root scope
-  '/',
-  `/${GHPATH}/index.html`,
-  `/${GHPATH}/ade-logo-white.png`,
-
   // current scope
-  `/${GHPATH}/`,
-  `/${GHPATH}/pwa-${GHPATH}-ico.png`,
-  `/${GHPATH}/index.js`,
-  `/${GHPATH}/assets/index.${CSS_PREFIX}.css`,
+  '/temp-calc/',
+  '/temp-calc/index.html',
+  '/temp-calc/ade-logo-white.png',
+  '/temp-calc/pwa-temp-calc-ico.png',
+  '/temp-calc/index.js',
+  '/temp-calc/assets/index.77388d6b.css',
 ];
