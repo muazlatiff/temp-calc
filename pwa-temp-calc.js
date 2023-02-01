b2d33cc33dc04f8b6c02ae7e76553841b60d5fbd
muazmohdlatiff <muazmohdlatiff@airasia.com>
2023-02-02 04:51:57 +0800

    1. FEAT: Pwa manifest - change all filename

---
+++ pwa-temp-calc.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+var GHPATH = 'temp-calc';
+var APP_PREFIX = 'tcpwa_';
+
+// every new deployment, need to update the prefix depending on new bundle filename
+var JS_PREFIX = '3b37ebd9';
+// every new deployment, need to update the prefix depending on new bundle filename
+var CSS_PREFIX = 'ddbb543e';
+
+var URLS = [
+  `/${GHPATH}/`,
+  `/${GHPATH}/index.html`,
+  `/${GHPATH}/ade-logo-white.png`,
+  `/${GHPATH}/pwa-${GHPATH}-ico.png`,
+  `/${GHPATH}/index.js`,
+  `/${GHPATH}/assets/index.${CSS_PREFIX}.css`,
+];
